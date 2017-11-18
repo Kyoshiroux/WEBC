ff5d98c4409c32bacffb26befebc15fffe86cac1
Kyoshiroux <30970476+Kyoshiroux@users.noreply.github.com>
2017-11-18 16:14:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,27 +9,16 @@ const request = require('request')
 const cheerio = require('cheerio')
 
 const urls = [
-    {
-        name: 'zuka',
-        url: 'http://rov.wikia.com/wiki/zuka'
-    },
-    {
-        name: 'murad',
-        url: 'http://rov.wikia.com/wiki/murad'
-    },
-    {
-       name: 'preya',
-       url: 'http://rov.wikia.com/wiki/preya'
-    }
+    {name: 'zuka', url: 'http://rov.wikia.com/wiki/zuka'},
+    {name: 'murad', url: 'http://rov.wikia.com/wiki/murad'},
+    {name: 'preya', url: 'http://rov.wikia.com/wiki/preya'}
 ]
 
-let i = 0
-
 const queue = async.queue((task, callback) =>{
-    request(task.url, (error, response, body)=>{
+    request(task.url, (error , response , body)=>{
         $ = cheerio.load(body)
         const text = $('#mw-content-text p').text()
-        fs.writeFile(task.name + ".txt",text, (err)=>{
+        fs.writeFile(task.name + ".txt", text , (err)=>{
             if(err) {
                 console.log(err)
                 callback()
